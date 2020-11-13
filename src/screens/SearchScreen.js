d7f53ecc10ead3aaf724ad0d8b928fc57cd0bc77
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-12 19:51:12 -0800

    Wire up SearchBar with SearchScreen component

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar'
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState('');
+
   return (
     <View>
-      <SearchBar />
+      <SearchBar
+        term={term}
+        onTermChange={(newTerm) => setTerm(newTerm)}
+        onTermSubmit={() => console.log('term was submitted')}
+      />
       <Text>Search Screen</Text>
+      <Text>{ term }</Text>
     </View>
   );
 };
