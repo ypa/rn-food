90793aefaff511c910b2f470f4871ad1ce4c7b4f
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-13 19:40:45 -0800

    Add error handling display error to user if something went wrong during yelp api call

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,16 +6,21 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const searchApi = async () => {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term: term,
-        location: 'Los Angeles'
-      }
-    });
-    setResults(response.data.businesses);
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term: term,
+          location: 'Los Angeles'
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage('Something went wrong');
+    }
   };
 
   return (
@@ -25,7 +30,7 @@ const SearchScreen = () => {
         onTermChange={setTerm}
         onTermSubmit={searchApi}
       />
-      <Text>Search Screen</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
   );
