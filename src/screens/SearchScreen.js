3d7cdd9d259e4933a63581afd84a80255e38b04e
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-13 19:57:24 -0800

    Add default search on the first time the component is rendered using useEffect

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar'
 import yelp from '../api/yelp';
@@ -8,12 +8,13 @@ const SearchScreen = () => {
   const [results, setResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const searchApi = async () => {
+  const searchApi = async (searchTerm) => {
+    console.log('hi there!');
     try {
       const response = await yelp.get('/search', {
         params: {
           limit: 50,
-          term: term,
+          term: searchTerm,
           location: 'Los Angeles'
         }
       });
@@ -23,12 +24,19 @@ const SearchScreen = () => {
     }
   };
 
+  // Call searchApi when component
+  // is first rendered
+  // searchApi('pasta');
+  useEffect(() => {
+    searchApi('pasta');
+  }, []);
+
   return (
     <View>
       <SearchBar
         term={term}
         onTermChange={setTerm}
-        onTermSubmit={searchApi}
+        onTermSubmit={() => searchApi(term)}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
