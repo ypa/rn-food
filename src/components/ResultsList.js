e51065925dc26c112244f648e41641fa64ede56f
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-15 17:31:23 -0800

    Refactor passing around navigation prop from parent to child to instead inject navigation with react-navigation's  function

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { withNavigation } from 'react-navigation';
 import ResultsDetail from './ResultsDetail';
 
 const ResultsList = ({ title, results, navigation }) => {
@@ -35,4 +36,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export default ResultsList;
+export default withNavigation(ResultsList);
